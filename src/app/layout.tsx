d35ea46eeb773405803c6a57ccabb828ade76555
systemcmd0122 <systemcmd02.address@gmail.com>
2024-06-22 08:59:16 +0900

    first

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "About System_Bot From_Discord",
+  description: "About System_Bot From_Discord",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
